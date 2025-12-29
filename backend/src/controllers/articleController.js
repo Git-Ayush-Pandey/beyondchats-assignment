654bab7ef7ea395f191aac44230738ae6e9f38e1
AP <ayushpandeyb@gmail.com>
2025-12-29 14:18:05 +0530

    feat: setup backend and article CRUD APIs

---
+++ backend/src/controllers/articleController.js
@@ -0,0 +1,20 @@
+const Article = require("../models/Article");
+
+exports.createArticle = async (req, res) => {
+  const article = await Article.create(req.body);
+  res.json(article);
+};
+
+exports.getArticles = async (req, res) => {
+  const articles = await Article.find();
+  res.json(articles);
+};
+
+exports.updateArticle = async (req, res) => {
+  const article = await Article.findByIdAndUpdate(
+    req.params.id,
+    req.body,
+    { new: true }
+  );
+  res.json(article);
+};
---
+++ backend/src/routes/articleRoutes.js
@@ -0,0 +1,8 @@
+const router = require("express").Router();
+const ctrl = require("../controllers/articleController");
+
+router.post("/", ctrl.createArticle);
+router.get("/", ctrl.getArticles);
+router.put("/:id", ctrl.updateArticle);
+
+module.exports = router;
